a137d70264f3844fa94d7e65d90d71048934658f
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-05-02 11:44:00 -0400

    add more log

--- script/DeployAndSetup.s.sol
+++ script/DeployAndSetup.s.sol
@@ -12,6 +12,10 @@ import "../src/mocks/ERC20Mintable.sol";
 */
 
 contract DeployAndSetupScript is Script {
+
+    event LogBalance(address token, address owner, uint256 balance);
+    event LogAllowance(address token, address owner, address spender, uint256 allowance);
+
     function setUp() public {}
 
     function run() public {
@@ -20,14 +24,23 @@ contract DeployAndSetupScript is Script {
         ZuniswapV2Factory factory = new ZuniswapV2Factory();
         ZuniswapV2Router router = new ZuniswapV2Router(address(factory));
 
+        address owner = address(this);
+        address spender = address(router);
+
         ERC20Mintable tokenA = new ERC20Mintable("Token 1", "TKN1");
         ERC20Mintable tokenB = new ERC20Mintable("Token 2", "TKN2");
 
-        tokenA.mint(20 ether, address(this));
-        tokenB.mint(20 ether, address(this));
+        tokenA.mint(20 ether, owner);
+        tokenB.mint(20 ether, owner);
 
-        tokenA.approve(address(router), 2 ether);
-        tokenB.approve(address(router), 2 ether);
+        emit LogBalance(address(tokenA), owner, tokenA.balanceOf(owner));
+        emit LogBalance(address(tokenB), owner, tokenB.balanceOf(owner));
+
+        tokenA.approve(spender, 2 ether);
+        tokenB.approve(spender, 2 ether);
+
+        emit LogAllowance(address(tokenA), owner, spender, tokenA.allowance(owner, spender));
+        emit LogAllowance(address(tokenB), owner, spender, tokenB.allowance(owner, spender));
 
         router.addLiquidity(
             address(tokenA),
@@ -36,7 +49,7 @@ contract DeployAndSetupScript is Script {
             1 ether,
             1 ether,
             1 ether,
-            address(this)
+            owner
         );
 
         vm.stopBroadcast();
